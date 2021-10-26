4535690e8ffc8c10a0789bf032f067883eaf1fb3
Jeff Hevrin <jhevrin2@gmail.com>
2021-10-26 09:59:22 -0500

    Updated to s3a

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     spark = get_spark_session()
     data = pd.read_csv("https://raw.githubusercontent.com/agconti/kaggle-titanic/master/data/train.csv")
     spark_df = spark.createDataFrame(data.select_dtypes(exclude=object))
-    spark_df.sample(0.5).write.mode('overwrite').orc("s3://jhevrin2-datasci/datascience/titanic/titanic-sample.orc")
+    spark_df.sample(0.5).write.mode('overwrite').orc("s3a://jhevrin2-datasci/datascience/titanic/titanic-sample.orc")
